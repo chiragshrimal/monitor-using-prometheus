57ea81534632c0c47a1661daa48ce4ebe0f15d81
chiragshrimal <chirag1711shrimal@gmail.com>
2025-08-01 15:10:26 +0530

    added histogram matric

--- index.ts
+++ index.ts
@@ -7,14 +7,6 @@ import client from "prom-client";
 
 const app=express();
 
-// create a gauge matric 
-// this gauge can be increase or decrease 
-// it can count only that req which are active mean which did not resposed till now
-const activeRequestsGauge = new client.Gauge({
-    name: 'active_requests',
-    help: 'Number of active requests'
-});
-
 // Create a counter metric
 // counter metric will always be increase 
 const requestCounter = new client.Counter({
@@ -23,6 +15,23 @@ const requestCounter = new client.Counter({
     // we dont have to added any labelNames which have infinite value because 
     // it can take many row 
     labelNames: ['method', 'route', 'status_code']
+});
+
+// create a gauge matric 
+// this gauge can be increase or decrease 
+// it can count only that req which are active mean which did not resposed till now
+const activeRequestsGauge = new client.Gauge({
+    name: 'active_requests',
+    help: 'Number of active requests'
+});
+
+// it store the count of request in the fashion of histogram 
+const httpRequestDurationMicroseconds = new client.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    // we have created some bucket and we always query how much request will less than something 
+    buckets: [0.1, 5, 15, 50, 100, 300, 500, 1000, 3000, 5000] // Define your own buckets here
 });
 
 function middleware(req:Request, res:Response, next:NextFunction){
@@ -34,6 +43,7 @@ function middleware(req:Request, res:Response, next:NextFunction){
 //  it is good because whenever in the route have any async function then it helps 
     res.on('finish', () => {
         const endTime = Date.now();
+        const duration = endTime-startTime;
         console.log(`Request took ${endTime - startTime}ms`);
 
         // Increment request counter
@@ -42,6 +52,13 @@ function middleware(req:Request, res:Response, next:NextFunction){
             route: req.route ? req.route.path : req.path,
             status_code: res.statusCode
         });
+
+        httpRequestDurationMicroseconds.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            code: res.statusCode
+        }, duration);
+
 
         activeRequestsGauge.dec();
     });
@@ -54,14 +71,14 @@ app.use(middleware);
 app.get("/cpu",async(req,res)=>{
 
     // it has to wait for 10 second so that we can see active request
-    await new Promise(s=>setTimeout(() => {
+    // await new Promise(s=>setTimeout(() => {
         
-    }, 10000));
+    // }, 10000));
 
     // this is cpu intensive task 
-    // for(let i=0;i<10000000000000;i++){
-    //     Math.random();
-    // }
+    for(let i=0;i<100000;i++){
+        Math.random();
+    }
 
     res.json({
         message : "cpu"
